7d42dcff2921fdd715bfb22509353b8989d30003
Phillip Jensen <phillip@golemgrid.com>
2021-06-26 22:57:38 +0200

    Upload files to container-manager-api

--- api-backend/api/views.py
+++ api-backend/api/views.py
@@ -1,11 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.conf import settings
 from .models import Blender
 import os
 from django.http import HttpResponse
 import json
 from rest_framework.parsers import MultiPartParser
 from rest_framework.decorators import parser_classes
+import requests
 
 
 @api_view(['POST'])
@@ -15,7 +17,8 @@ def create_blender_task(request):
         scene = request.FILES['scene_file']
         construct_json = {
             "scene_file": "/golem/resource/" + request.FILES['scene_file'].name,
-            "resolution": [request.data["resolutionx"], request.data["resolutiony"]],
+            "resolution1": request.data["resolutionx"],
+            "resolution2":  request.data["resolutiony"],
             "use_compositing": request.data["compositing"],
             "crops": [request.data["borderleft"], request.data["borderright"], request.data["borderup"], request.data["borderdown"]],
             "samples": request.data["samples"],
@@ -25,5 +28,10 @@ def create_blender_task(request):
             "WORK_DIR": "/golem/work",
             "OUTPUT_DIR": "/golem/output",
         }
-        Blender.objects.create(task_args=json.dumps(
+        e = Blender.objects.create(task_args=json.dumps(
             construct_json), scene_file=scene)
+        url = "http://container-manager-api:8003/v1/start/blender"
+        files = {'file': open(
+            settings.MEDIA_ROOT + str(e.scene_file), 'rb')}
+
+        r = requests.post(url, files=files, data=construct_json)
