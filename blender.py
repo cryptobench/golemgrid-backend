7be8e9a7b6b71c7b016b772f852d210e4a6f381c
ControlCplusControlV <44706811+ControlCplusControlV@users.noreply.github.com>
2021-07-20 15:03:47 -0700

    Update blender.py

--- blender.py
+++ blender.py
@@ -1,57 +1,28 @@
 #!/usr/bin/env python3
-# Start command
-'''
---json {
-    "scene_file": "/golem/resource/scene.blend",
-    "resolution": (400, 300),
-    "use_compositing": False,
-    "crops": crops,
-    "samples": 100,
-    "frames": [frame],
-    "output_format": "PNG",
-    "RESOURCES_DIR": "/golem/resources",
-    "WORK_DIR": "/golem/work",
-    "OUTPUT_DIR": "/golem/output",
-}
---crops [{"outfilebasename": "out", "borders_x": [0.0, 1.0], "borders_y": [0.0, 1.0]}]
-'''
 import asyncio
 from datetime import datetime, timedelta
 import pathlib
 import sys
-import argparse
 import json
+import argparse
 from yapapi import (
-    Executor,
+    Golem,
     NoPaymentAccountError,
     Task,
     __version__ as yapapi_version,
     WorkContext,
     windows_event_loop_fix,
 )
-from yapapi.log import enable_default_logger, log_summary, log_event_repr  # noqa
-from yapapi.package import vm
+from yapapi.log import enable_default_logger
+from yapapi.payload import vm
 from yapapi.rest.activity import BatchTimeoutError
 
 examples_dir = pathlib.Path(__file__).resolve().parent.parent
 sys.path.append(str(examples_dir))
 
-from utils import (
-    build_parser,
-    TEXT_COLOR_CYAN,
-    TEXT_COLOR_DEFAULT,
-    TEXT_COLOR_RED,
-    TEXT_COLOR_YELLOW,
-)
-#This isn't that elegant, but its a lot cleaner and argparser was being very annoying doing it indivudally
-def str_to_bool(s):
-    if s == 'True':
-        return True
-    elif s == 'False':
-        return False
-    else:
-        raise ValueError
-async def main(subnet_tag, driver=None, network=None):
+
+
+async def main(params, subnet_tag, driver=None, network=None):
     package = await vm.repo(
         image_hash="9a3b5d67b0b27746283cb5f287c13eab1beaa12d92a9f536b747c7ae",
         min_mem_gib=0.5,
@@ -64,19 +35,32 @@ async def main(subnet_tag, driver=None, network=None):
         ctx.send_file(scene_path, "/golem/resource/scene.blend")
         async for task in tasks:
             frame = task.data
-            crops = args.crops
+            crops = [{"outfilebasename": "out", "borders_x": [0.0, 1.0], "borders_y": [0.0, 1.0]}]
             ctx.send_json(
-                "/golem/work/params.json",finalized_packet,
+                "/golem/work/params.json",
+                {
+                    "scene_file": params['scene_file'],
+                    "resolution": (params['resolution1'], params['resolution2']),
+                    "use_compositing": params['use_compositing'],
+                    "crops": crops,
+                    "samples": params['samples'],
+                    "frames": [frame],
+                    "output_format": params['output_format'],
+                    "RESOURCES_DIR": params['RESOURCES_DIR'],
+                    "WORK_DIR": params['WORK_DIR'],
+                    "OUTPUT_DIR": params['OUTPUT_DIR'],
+                },
             )
             ctx.run("/golem/entrypoints/run-blender.sh")
             output_file = f"output_{frame}.png"
             ctx.download_file(f"/golem/output/out{frame:04d}.png", output_file)
             try:
-                # Set timeout for executing the script on the provider. Two minutes is plenty
-                # of time for computing a single frame, for other tasks it may be not enough.
-                # If the timeout is exceeded, this worker instance will be shut down and all
-                # remaining tasks, including the current one, will be computed by other providers.
-                yield ctx.commit(timeout=timedelta(seconds=120))
+                # Set timeout for executing the script on the provider. Usually, 30 seconds
+                # should be more than enough for computing a single frame, however a provider
+                # may require more time for the first task if it needs to download a VM image
+                # first. Once downloaded, the VM image will be cached and other tasks that use
+                # that image will be computed faster.
+                yield ctx.commit(timeout=timedelta(minutes=10))
                 # TODO: Check if job results are valid
                 # and reject by: task.reject_task(reason = 'invalid file')
                 task.accept_result(result=output_file)
@@ -96,75 +80,59 @@ async def main(subnet_tag, driver=None, network=None):
     # Providers will not accept work if the timeout is outside of the [5 min, 30min] range.
     # We increase the lower bound to 6 min to account for the time needed for our demand to
     # reach the providers.
-    min_timeout, max_timeout = 6, 30
+    min_timeout, max_timeout = 6, 29
 
     timeout = timedelta(minutes=max(min(init_overhead + len(frames) * 2, max_timeout), min_timeout))
 
-    # By passing `event_consumer=log_summary()` we enable summary logging.
-    # See the documentation of the `yapapi.log` module on how to set
-    # the level of detail and format of the logged information.
-    async with Executor(
-        package=package,
-        max_workers=3,
+    async with Golem(
         budget=10.0,
-        timeout=timeout,
         subnet_tag=subnet_tag,
         driver=driver,
         network=network,
-        event_consumer=log_summary(log_event_repr),
-    ) as executor:
+    ) as golem:
 
         print(
-            f"yapapi version: {TEXT_COLOR_YELLOW}{yapapi_version}{TEXT_COLOR_DEFAULT}\n"
-            f"Using subnet: {TEXT_COLOR_YELLOW}{subnet_tag}{TEXT_COLOR_DEFAULT}, "
-            f"payment driver: {TEXT_COLOR_YELLOW}{executor.driver}{TEXT_COLOR_DEFAULT}, "
-            f"and network: {TEXT_COLOR_YELLOW}{executor.network}{TEXT_COLOR_DEFAULT}\n"
+            f"yapapi version: {yapapi_version}\n"
+            f"Using subnet: {subnet_tag}, "
+            f"payment driver: {golem.driver}, "
+            f"and network: {golem.network}\n"
         )
 
         num_tasks = 0
         start_time = datetime.now()
 
-        async for task in executor.submit(worker, [Task(data=frame) for frame in frames]):
+        completed_tasks = golem.execute_tasks(
+            worker,
+            [Task(data=frame) for frame in frames],
+            payload=package,
+            max_workers=3,
+            timeout=timeout,
+        )
+        async for task in completed_tasks:
             num_tasks += 1
             print(
-                f"{TEXT_COLOR_CYAN}"
                 f"Task computed: {task}, result: {task.result}, time: {task.running_time}"
-                f"{TEXT_COLOR_DEFAULT}"
             )
 
         print(
-            f"{TEXT_COLOR_CYAN}"
             f"{num_tasks} tasks computed, total time: {datetime.now() - start_time}"
-            f"{TEXT_COLOR_DEFAULT}"
         )
 
 
 if __name__ == "__main__":
     now = datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
-    log_file=f"blender-yapapi-{now}.log"
-    #Old parser was above
-    parser = argparse.ArgumentParser()# Add an argument
-    parser.add_argument('-n','--name', type=str, required=True)# Parse the argument
-    args = parser.parse_args()# Print "Hello" + the user input argument
-    argpacket = json.loads(args.name)
-    composting = [{"outfilebasename": argpacket["outfilebasename"], "borders_x": [float(argpacket["borderx1"]), float(argpacket["borderx2"])], "borders_y": [float(argpacket["bordery1"]), float(argpacket["bordery2"])]}]
-    finalized_packet = {
-    "scene_file": argpacket["scene_file"],
-    "resolution": (int(argpacket["resolution1"]), 
-    int(argpacket["resolution2"])),
-    "use_compositing": str_to_bool(argpacket["use_compositing"]),
-    "crops" : composting,
-    "samples": int(argpacket["samples"]),
-    "output_format": argpacket["output_format"],
-    "RESOURCES_DIR": argpacket["RESOURCES_DIR"],
-    "WORK_DIR": argpacket["WORK_DIR"],
-    "OUTPUT_DIR": argpacket["OUTPUT_DIR"]
-    }
+
     # This is only required when running on Windows with Python prior to 3.8:
     windows_event_loop_fix()
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-j','--jpath', type=str, required=True)
+    args = parser.parse_args()
+    jsonParams = open(args.jpath,) 
+    # returns JSON object as 
+    # a dictionary
+    params = json.load(jsonParams)
     enable_default_logger(
-        log_file=log_file,
+        log_file=f"blender-yapapi-{now}.log",
         debug_activity_api=True,
         debug_market_api=True,
         debug_payment_api=True,
@@ -172,7 +140,7 @@ if __name__ == "__main__":
 
     loop = asyncio.get_event_loop()
     task = loop.create_task(
-        main(subnet_tag="devnet-beta.1", driver="zksync", network="rinkeby")
+        main(params,subnet_tag="devnet-beta.2", driver="zksync", network="rinkeby")
     )
 
     try:
@@ -183,24 +151,20 @@ if __name__ == "__main__":
             "flash-tutorial-of-requestor-development"
         )
         print(
-            f"{TEXT_COLOR_RED}"
             f"No payment account initialized for driver `{e.required_driver}` "
             f"and network `{e.required_network}`.\n\n"
             f"See {handbook_url} on how to initialize payment accounts for a requestor node."
-            f"{TEXT_COLOR_DEFAULT}"
         )
     except KeyboardInterrupt:
         print(
-            f"{TEXT_COLOR_YELLOW}"
             "Shutting down gracefully, please wait a short while "
             "or press Ctrl+C to exit immediately..."
-            f"{TEXT_COLOR_DEFAULT}"
         )
         task.cancel()
         try:
             loop.run_until_complete(task)
             print(
-                f"{TEXT_COLOR_YELLOW}Shutdown completed, thank you for waiting!{TEXT_COLOR_DEFAULT}"
+                f"Shutdown completed, thank you for waiting!"
             )
         except (asyncio.CancelledError, KeyboardInterrupt):
             pass
