b7cb3fd0e9637b934432a977c05cd83955d28973
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2021-12-13 14:36:34 +0100

    Use event consumer

--- compose/requestor/start.sh
+++ compose/requestor/start.sh
@@ -6,8 +6,8 @@ echo "Starting Yagna"
 RUST_LOG=error MARKET_DB_CLEANUP_INTERVAL=10min /root/.local/bin/yagna service run > /dev/null 2>&1 &
 sleep 5
 key=$(/root/.local/bin/yagna app-key create requester)
-/root/.local/bin/yagna payment fund
-/root/.local/bin/yagna payment init --sender
+/root/.local/bin/yagna payment fund --driver zksync
+/root/.local/bin/yagna payment init --sender --driver zksync
 echo "Installing custom yapapi"
-pip3 install git+https://github.com/cryptobench/yapapi.git@b0.7
+pip3 install git+https://github.com/golemfactory/yapapi.git
 echo "Finished installing yapapi"
