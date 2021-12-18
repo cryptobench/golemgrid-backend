db7e9f28cbd0e547b30b8fd8b3af445da9edb7b6
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2021-12-18 11:53:29 +0100

    Remove unnecessary params that are not used

--- api-backend/api/views.py
+++ api-backend/api/views.py
@@ -18,18 +18,7 @@ from django.contrib.auth.decorators import login_required
 def create_blender_task(request):
     if request.method == 'POST':
         scene = request.FILES['scene_file']
-        construct_json = {
-            "resolution1": request.data["resolutionx"],
-            "resolution2":  request.data["resolutiony"],
-            "use_compositing": request.data["compositing"],
-            "crops": [request.data["borderleft"], request.data["borderright"], request.data["borderup"], request.data["borderdown"]],
-            "samples": request.data["samples"],
-            "frames": request.data["frames"],
-            "output_format": request.data["output"],
-            "RESOURCES_DIR": "/golem/resources",
-            "WORK_DIR": "/golem/work",
-            "OUTPUT_DIR": "/golem/output",
-        }
+        construct_json = {}
         e = Blender.objects.create(task_args=json.dumps(
             construct_json), scene_file=scene, user=request.user)
         construct_json["scene_file"] = str(e.scene_file),
